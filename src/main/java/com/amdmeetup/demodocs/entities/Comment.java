0e4cdf135af2c233faecf20a242b735a80a1fa60
radbrawler <radbrawler@gmail.com>
2018-03-08 23:20:53 +0530

    Add repository for crud operations through rest api

--- src/main/java/com/amdmeetup/demodocs/entities/Comment.java
+++ src/main/java/com/amdmeetup/demodocs/entities/Comment.java
@@ -9,7 +9,7 @@ import javax.persistence.OneToOne;
 
 @Entity
 @Data
-public class Comments {
+public class Comment {
     @Id
     @GeneratedValue
     Long id;
---
+++ src/main/java/com/amdmeetup/demodocs/entities/TweetRepository.java
@@ -0,0 +1,6 @@
+package com.amdmeetup.demodocs.entities;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface TweetRepository extends CrudRepository<Tweet, Long> {
+}
