a31c29744e4ad43bf1e0597d953d1c54ef42132f
Vicky Ajmera <vicky.ajmera@ishisystems.com>
2018-03-10 12:29:52 +0530

    Serving Spring data REST docs through application.

--- src/main/java/com/amdmeetup/demodocs/config/WebMvcConfig.java
+++ src/main/java/com/amdmeetup/demodocs/config/WebMvcConfig.java
@@ -1,7 +1,22 @@
 package com.amdmeetup.demodocs.config;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
+
+  private static final String[] CLASSPATH_RESOURCE_LOCATIONS = {
+      "classpath:/META-INF/resources/",
+      "classpath:/resources/",
+      "classpath:/static/",
+      "classpath:/public/"
+  };
+
+  @Override
+  public void addResourceHandlers(ResourceHandlerRegistry registry) {
+    registry.addResourceHandler("/**").addResourceLocations(CLASSPATH_RESOURCE_LOCATIONS);
+  }
 
 }
